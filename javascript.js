822aa108b7b1d5e30002defaaa7677db6dd9f4ef
Vasu Sharma <vsh.9705@gmail.com>
2024-06-26 02:35:46 +0530

    DOM Manipulation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const result = document.querySelector("#result");
+const winner = document.querySelector("#winner");
+
 var humanScore = 0;
 var computerScore = 0;
 
@@ -14,62 +20,84 @@ function getComputerChoice() {
     return c_choice;
 }
 
-function getHumanChoice() {
-    var input = prompt("Select: Rock/Paper/Scissors");
-    return input.toLowerCase();
-}
+// function getHumanChoice() {
+//     var input = prompt("Select: Rock/Paper/Scissors");
+//     return input.toLowerCase();
+// }
 
-function playRound() {
-    var humanChoice = getHumanChoice();
+function playRound(humanChoice) {
     var computerChoice = getComputerChoice();
 
     if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        console.log(`You win! ${humanChoice} beats ${computerChoice} `);
-        humanScore++;
-    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice} `);
-        computerScore++;
-    } else if (humanChoice === 'rock' && computerChoice === 'rock') {
-        console.log(`Tie!`);
-    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        console.log(`You win! ${humanChoice} beats ${computerChoice} `);
-        humanScore++;
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice} `);
-        computerScore++;
-    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
-        console.log(`Tie!`);
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice} `);
-        computerScore++;
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log(`You win! ${humanChoice} beats ${computerChoice} `);
-        humanScore++;
-    } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
-        console.log(`Tie!`);
+        humanScore ++;
+        result.textContent = `You win! ${humanChoice} beats ${computerChoice} Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore ++;
+        result.textContent = `You lose! ${computerChoice} beats ${humanChoice} Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'rock' && computerChoice === 'rock') {
+        result.textContent = `Tie! Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        humanScore ++;
+        result.textContent = `You win! ${humanChoice} beats ${computerChoice} Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    }
+     else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        computerScore ++;
+        result.textContent = `You lose! ${computerChoice} beats ${humanChoice} Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'paper' && computerChoice === 'paper') {
+        result.textContent = `Tie! Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore ++;
+        result.textContent = `You lose! ${computerChoice} beats ${humanChoice} Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore ++;
+        result.textContent = `You win! ${humanChoice} beats ${computerChoice} Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } 
+    else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+        result.textContent = `Tie! Your Score: ${humanScore} Computer Score: ${computerScore}`;
     }
 
-    console.log(`Human Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
+    if(humanScore === 5){
+        winner.textContent = "You win the game!"
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if(computerScore === 5){
+        winner.textContent = "Computer wins the game!"
+        humanScore = 0;
+        computerScore = 0;
+    }
 }
 
-function playGame(){
-    for(var i = 0 ; i < 5 ; i++){
-        playRound();
+rock.addEventListener('click', (event)=>{
+    if(humanScore === 0 && computerScore === 0){
+        winner.textContent = "";
     }
-    if(humanScore > computerScore){
-        console.log("You win the game!")
-    }
-    else if (humanScore < computerScore){
-        console.log("You lose the game!")
-    }
-    else{
-        console.log("The game is a tie!")
-    }
+    result.textContent = "";
+    var humanChoice = "rock";
+    playRound(humanChoice);
+})
 
-    console.log(`Final Human Score: ${humanScore}`);
-    console.log(`Final Computer Score: ${computerScore}`);
-}
+paper.addEventListener('click', (event)=>{
+    if(humanScore === 0 && computerScore === 0){
+        winner.textContent = "";
+    }
+    result.textContent = "";
+    var humanChoice = "paper";
+    playRound(humanChoice);
+})
 
-playGame();
+scissors.addEventListener('click', (event)=>{
+    if(humanScore === 0 && computerScore === 0){
+        winner.textContent = "";
+    }
+    result.textContent = "";
+    var humanChoice = "scissors";
+    playRound(humanChoice);
+})
 
